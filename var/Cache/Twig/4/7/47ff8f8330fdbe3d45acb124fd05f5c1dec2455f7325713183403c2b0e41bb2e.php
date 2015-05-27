df6c3cd63ed4e8e43235dad5a6edd5cdbf15a701
Nigel Greenway <nigel_greenway@me.com>
2015-05-27 13:53:28 +0100

    Correct permissions

---
+++ var/Cache/Twig/4/7/47ff8f8330fdbe3d45acb124fd05f5c1dec2455f7325713183403c2b0e41bb2e.php
@@ -0,0 +1,51 @@
+<?php
+
+/* @Welcome/hello.html.twig */
+class __TwigTemplate_47ff8f8330fdbe3d45acb124fd05f5c1dec2455f7325713183403c2b0e41bb2e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("@Base/base.html.twig", "@Welcome/hello.html.twig", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "@Base/base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        // line 4
+        echo "<p>Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo ", how are you?</p>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@Welcome/hello.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,  11 => 1,);
+    }
+}
