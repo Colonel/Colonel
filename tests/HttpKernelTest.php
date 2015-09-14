27eb1faf60836ca7f56d2b4d77f0fafc09b84410
Nigel Greenway <nigel_greenway@me.com>
2015-09-14 10:23:04 +0100

    Add initial Service Provider to core

--- tests/HttpKernelTest.php
+++ tests/HttpKernelTest.php
@@ -9,6 +9,7 @@
 namespace Colonel\Test\Configuration;
 
 use Colonel\HttpKernel;
+use Colonel\Test\Fixtures\TestServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -49,5 +50,46 @@ class HttpKernelTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals('<h1>It works!</h1>', $response->getContent());
+    }
+
+    /**
+     * @covers \Colonel\HttpKernel::handle
+     * @covers \Colonel\HttpKernel::run
+     */
+    public function test_handle_is_successful_with_a_service_provider()
+    {
+        $app = new HttpKernel([
+            'debug' => false,
+            'services' => [
+                'di' => [
+                    // Leave empty
+                ],
+            ],
+            'routes' => [
+                'test_group' => [
+                    'test_route' => [
+                        'pattern'    => '',
+                        'controller' => function() {
+                            return Response::create('<h1>It works!</h1>', 200);
+                        },
+                        'method'     => 'GET',
+                    ],
+                ],
+            ],
+            'service_providers' => [
+                TestServiceProvider::class => new TestServiceProvider()
+            ],
+        ]);
+
+        $request = Request::createFromGlobals();
+
+        $response = $app->handle(
+            $request,
+            1,
+            true
+        );
+
+        $this->assertEquals('<h1>It works!</h1>', $response->getContent());
+        $this->assertTrue($app->container->isSingleton(\stdClass::class));
     }
 }
