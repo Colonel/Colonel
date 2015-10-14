19d77a6cc589d0e9d25d58e35d7b07501b61b040
Nigel Greenway <nigel_greenway@me.com>
2015-10-14 10:33:09 +0100

    Extract test for the service provider feature into its own test

--- tests/HttpKernelTest.php
+++ tests/HttpKernelTest.php
@@ -55,45 +55,4 @@ class HttpKernelTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('<h1>It works!</h1>', $response->getContent());
     }
-
-    /**
-     * @covers \Colonel\HttpKernel::handle
-     * @covers \Colonel\HttpKernel::run
-     */
-    public function test_handle_is_successful_with_a_service_provider()
-    {
-        $app = new HttpKernel([
-            'debug' => false,
-            'services' => [
-                'di' => [
-                    // Leave empty
-                ],
-            ],
-            'routes' => [
-                'test_group' => [
-                    'test_route' => [
-                        'pattern'    => '/',
-                        'controller' => function() {
-                            return Response::create('<h1>It works!</h1>', 200);
-                        },
-                        'method'     => 'GET',
-                    ],
-                ],
-            ],
-            'service_providers' => [
-                TestServiceProvider::class => new TestServiceProvider()
-            ],
-        ]);
-
-        $request = Request::createFromGlobals();
-
-        $response = $app->handle(
-            $request,
-            1,
-            true
-        );
-
-        $this->assertEquals('<h1>It works!</h1>', $response->getContent());
-        $this->assertTrue($app->container->isSingleton(\stdClass::class));
-    }
 }
